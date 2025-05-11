bc80a05a4c85b8b69cfd1557215d73ffa01b8cc5
Viniciusovski <vini98.2012@gmail.com>
2025-05-11 00:03:35 -0300

    Criando componentes através da CLI

--- conceitos-basicos/src/app/app.component.ts
+++ conceitos-basicos/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
 import { HelloWorldComponent } from './helloworld/hellowordl.component';
-
+// importa os componentes criados para ser possivel utiliza-los
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [RouterOutlet, HelloWorldComponent, MinhapaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
