53363c0c225d05b62ba23f06316f7ebcc9195671
Viniciusovski <vini98.2012@gmail.com>
2025-05-15 17:19:08 -0300

    Router Link e Material Icons

--- crud-angular-material/src/app/app.component.ts
+++ crud-angular-material/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon';
+
+/*
+O RouterLink é um componente do Angular que permite 
+criar links para navegar entre rotas de uma aplicação.
+Ele é usado para criar links que, quando clicados, 
+ativam a navegação para uma rota específica da 
+aplicação, em vez de carregar uma nova página.
+*/
 
 @Component({
   selector: 'app-root',
   // RouterOutlet é o roteador da aplicação que é usada para acessar as rotas
   // Onde coloca o RouterOutlet é onde as páginas vão aparecer
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
